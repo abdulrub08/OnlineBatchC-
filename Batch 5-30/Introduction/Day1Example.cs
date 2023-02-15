525fe7cb63fe0ef603118ceee26e9e6bef84b11a
Abdul <abdulrub44469@gmail.com>
2023-02-15 19:17:00 +0530

    Added File Day1

---
+++ Batch 5-30/Introduction/Day1Example.cs
@@ -0,0 +1,10 @@
+using System;  				
+class Test
+{
+		static void Main()
+		{
+			string _name="Hello";
+			Console.WriteLine(_name);
+			Console.ReadLine();
+		}
+}
---
+++ Batch 5-30/Introduction/MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//" // This is called Double quotes
+//' // This is called Single Quotes
+string _name = "$%^A1233";
+char _char = 'A';
+int age = 24;
+float salary = 10500.20f;
+//DataType Name;
+//DataType Name="";
+//string fullName = "Abdul Rub";
+Console.WriteLine("Please Enter Your Full Name");
+string fullName = Console.ReadLine();
+Console.WriteLine("Full Name:- " + fullName);
+Console.WriteLine("Name:- " + _name);
+Console.WriteLine("Char Value:- " + _char);
+Console.WriteLine("Age:- " + age);
+Console.WriteLine("Salary:- " + salary);
