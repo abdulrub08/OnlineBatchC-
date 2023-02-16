68548426502a3cd098542c4cc08a301bfdcf512e
Abdul <abdulrub44469@gmail.com>
2023-02-16 22:58:34 +0530

    .gitignore is now working

---
+++ Batch 5-30/Day2/Calculation/Calculation/MakeSquare.cs
@@ -0,0 +1,17 @@
+﻿namespace AbdulRub
+{
+    public class MakeSquare
+    {
+        public int a;
+        public void EnaterNValue()
+        {
+            Console.WriteLine("Please Enter (N) Value");
+            string val = Console.ReadLine();
+            a = int.Parse(val);
+        }
+        public void DisplayNSquare()
+        {
+            Console.WriteLine("N Square Value is "+ (a*a));
+        }
+    }
+}
---
+++ Batch 5-30/Day2/Calculation/Calculation/Program.cs
@@ -0,0 +1,51 @@
+﻿using AbdulRub;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+MakeSquare obj = new MakeSquare();
+obj.EnaterNValue();
+obj.DisplayNSquare();
+Console.Read();
+
+InputTwoNumberFromUser();
+Add();
+DisplayTotalNumber();
+Console.Read();
+string firstval;
+string secondval;
+Int64 sum;
+
+//ReturnType FunctiName()
+//{//Start
+//Statement Code
+//}//End
+//Example
+
+void InputTwoNumberFromUser()
+{    
+    Console.WriteLine("Please Enter A First Number");
+    firstval = Console.ReadLine();
+    
+    Console.WriteLine("Please Enter A Second Number");
+    secondval = Console.ReadLine();
+}
+
+void Add()
+{
+    sum = Convert.ToInt64(firstval) + Convert.ToInt64(secondval);
+}
+
+void DisplayTotalNumber()
+{
+    Console.WriteLine(sum);
+}
+
+
+//Avarage A+B+C/3
+
+//AirthMatic Operator
+//- + % * /
+
+
+
